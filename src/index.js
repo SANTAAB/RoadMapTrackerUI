6d68ca6a7dc9f5a56913b4cfd63657fe5742e4fd
Alexander Byrka <alexander.byrka@sigma.software>
2019-10-06 23:50:12 +0300

    pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
 import Dashboard from './Dashboard';
+import React from 'react'
+import { render } from 'react-dom'
+import {
+      HashRouter,
+      Route
+    } from 'react-router-dom'
+
+    window.React = React
+render( 
+  <HashRouter>
+    <div className="main">
+      <Route exact path="/" component={Dashboard} /> 
+    </div>
+  </HashRouter>,
+  document.getElementById('root')
+  //document.getElementById('react-container')
+)
 
 
 
-
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+//ReactDOM.render(<Dashboard />, document.getElementById('root'));
