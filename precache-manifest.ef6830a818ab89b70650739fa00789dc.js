4aafba425d1de36118e4dcb65c824f47a8672234
Alexander Byrka <alexander.byrka@sigma.software>
2019-10-06 23:05:47 +0300

    Updates

---
+++ precache-manifest.ef6830a818ab89b70650739fa00789dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0316c3d45688edabeeb05000ee8f6a5d",
+    "url": "/RoadMapTrackerUI/index.html"
+  },
+  {
+    "revision": "c34be9a3b66ab2e546fd",
+    "url": "/RoadMapTrackerUI/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "120cb897a81caedba37e",
+    "url": "/RoadMapTrackerUI/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c34be9a3b66ab2e546fd",
+    "url": "/RoadMapTrackerUI/static/js/2.d8a2a8c6.chunk.js"
+  },
+  {
+    "revision": "120cb897a81caedba37e",
+    "url": "/RoadMapTrackerUI/static/js/main.76b9356e.chunk.js"
+  },
+  {
+    "revision": "026f9892b19a2a120a02",
+    "url": "/RoadMapTrackerUI/static/js/runtime-main.7ea976df.js"
+  }
+]);
